34c85e613f517013cac2b5ca95a3d0470b3ca5b0
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-10-20 23:32:03 +0000

    feat: add getTodos, addTodos, completeTodos, deleteTodos logic for handling todos logic

--- src/store/todoStore.ts
+++ src/store/todoStore.ts
@@ -1,28 +1,94 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 import { create } from "zustand";
+import supabase from "../configs/supabaseClient";
 
 type TodosTypes = {
   id: number;
   task: string;
-  is_complete: false;
+  is_complete: boolean;
 };
 
 type State = {
+  // task: string;
+  // completed: boolean;
+  errors: string;
   todos: TodosTypes[];
 };
 
 type Actions = {
-  addTodos: () => void;
-  deleteTodos: () => void;
-  getTodos: () => void;
-  completeTodos: () => void;
+  addTodos: (task: string) => Promise<void>;
+  deleteTodos: (id: number) => Promise<void>;
+  getTodos: () => Promise<void>;
+  completeTodos: (id: number, isCompleted: boolean) => Promise<void>;
 };
 
 const useTodoStore = create<State & Actions>((set) => ({
+  // task: "",
+  // completed: false,
+  errors: "",
   todos: [],
-  getTodos: () => {},
-  addTodos: () => {},
-  deleteTodos: () => {},
-  completeTodos: () => {},
+  getTodos: async (): Promise<void> => {
+    try {
+      const { data, error } = await supabase
+        .from("todos")
+        .select("*")
+        .order("id", { ascending: false });
+      if (error) console.log("Errors - Fetch Todos", error);
+      if (data != null) {
+        set(() => ({ todos: data as TodosTypes[] }));
+      }
+    } catch (error) {
+      alert(error);
+      console.log("Errors - Fetch Todos", error);
+    }
+  },
+  addTodos: async (task: string): Promise<void> => {
+    if (task.length <= 3) {
+      set(() => ({ errors: "Task length should be more than 3!" }));
+    } else {
+      const { data, error } = await supabase
+        .from("todos")
+        .insert({ task: task })
+        .single();
+      if (error) console.log("Error - Add Todos", error.message);
+      if (data != null) {
+        set((state) => ({
+          todos: [data, ...state.todos],
+        })),
+          set(() => ({ errors: "" }));
+        task = "";
+      }
+    }
+  },
+  deleteTodos: async (id: number): Promise<void> => {
+    try {
+      const { error } = await supabase.from("todos").delete().eq("id", id);
+      set((state) => ({
+        todos: state.todos.filter((todo) => todo.id !== id),
+      }));
+      if (error) {
+        console.error(error);
+      }
+      window.location.reload();
+    } catch (error) {
+      console.log("Error - Delete Todos", error);
+    }
+  },
+  completeTodos: async (id: number, isCompleted: boolean): Promise<void> => {
+    try {
+      const { error } = await supabase
+        .from("todos")
+        .update({ is_complete: isCompleted })
+        .eq("id", id)
+        .single();
+      if (error) {
+        console.error(error);
+      }
+      window.location.reload();
+    } catch (error) {
+      console.log("Error - Complete Todos", error);
+    }
+  },
 }));
 
 export default useTodoStore;
