d34c237df145251de8b76a1b755572d8a1882f66
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-10-20 21:01:07 +0000

    config: add the supabase configuration for url & anon key

---
+++ src/configs/supabaseClient.ts
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from "./constants";
+
+const supabaseUrl = SUPABASE_URL;
+const supabaseAnonKey = SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export default supabase;
