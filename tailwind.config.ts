9e8e4794d94d602be296828516175af7faf189c0
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-10-21 23:57:19 +0000

    config: add the colors, font-size & font-weight configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,26 @@ export default {
       mosk: ["Mosk", "sans-serif"],
     },
 
-    extend: {},
+    fontSize: {
+      "fs-logo": "var(--fs-logo)",
+      "fs-base": "var(--fs-base)",
+      "fs-small": "var(--fs-logo)",
+    },
+
+    fontWeight: {
+      "fw-bold": "var(--fw-bold)",
+      "fw-regular": "var(--fw-regular)",
+    },
+
+    extend: {
+      colors: {
+        "color-primary-light": "var(--color-primary-light)",
+        "color-secondary-light": "var(--color-secondary-light)",
+        "color-body-background-light": "var(--color-body-background-light)",
+        "color-elements-light": "var(--color-elements-light)",
+        "color-text-light": "var(--color-text-light)",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
