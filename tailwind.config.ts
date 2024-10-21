01ed90752b71923d48b88d7ea2a1f22a8ff1b12b
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-10-21 23:23:26 +0000

    config: update the tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,21 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+    },
+
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+
+    fontFamily: {
+      mosk: ["Mosk", "sans-serif"],
+    },
+
     extend: {},
   },
   plugins: [],
