8a0cec46eaceff3c5a241fb5a96b5c003d06d687
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-10-23 01:17:01 +0000

    fix: fix the hero image import issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
+import heroImg from "/src/assets/images/hero-img.jpg";
+
 const App: React.FC = () => {
   return (
     <React.Fragment>
       <div className="hero-img | absolute top-0 left-0 -z-10  w-full h-[360px]">
         <img
-          src="./src/assets/images/hero-img.jpg"
+          src={heroImg}
           alt="Beautifull Mountain and rivers"
           className="bg-no-repeat bg-cover w-full h-full"
         />
